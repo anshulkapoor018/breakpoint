1a03619d0cada0d3147c7e5d6fba83c232e83d1a
Anshul Kapoor <anshulkapoor018@gmail.com>
2017-10-23 18:33:18 +0530

    Fetching Messages

---
+++ breakpoint/Model/Message.swift
@@ -0,0 +1,26 @@
+//
+//  Message.swift
+//  breakpoint
+//
+//  Created by Anshul Kapoor on 23/10/17.
+//  Copyright © 2017 Anshul Kapoor. All rights reserved.
+//
+
+import Foundation
+
+class Message{
+    private var _content : String
+    private var _senderId : String
+    
+    var content : String{
+        return _content
+    }
+    var senderId : String{
+        return _senderId
+    }
+    
+    init(content: String, senderId: String) {
+        self._content = content
+        self._senderId = senderId
+    }
+}
---
+++ breakpoint/View/FeedCell.swift
@@ -0,0 +1,21 @@
+//
+//  FeedCell.swift
+//  breakpoint
+//
+//  Created by Anshul Kapoor on 23/10/17.
+//  Copyright © 2017 Anshul Kapoor. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    
+    func configureCell(profileImage: UIImage, email: String, content: String){
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+    }
+}
