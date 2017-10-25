d490a63c47fe56115d4842bf4675e9c1fc0f8901
Anshul Kapoor <anshulkapoor018@gmail.com>
2017-10-25 16:00:04 +0530

    Final fixes

--- breakpoint/Controller/GroupFeedVC.swift
+++ breakpoint/Controller/GroupFeedVC.swift
@@ -67,7 +67,7 @@ class GroupFeedVC: UIViewController {
     }
     
     @IBAction func backBtnWasPressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+        dismissDetail()
     }
 }
 
